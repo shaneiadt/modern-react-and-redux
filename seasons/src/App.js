ccf7e108c920ed666f5aeadeeb2f292d7a304b5a
Shane O'Moore <shaneiadt@gmail.com>
2021-10-15 15:10:13 +0100

    dump geolocation on screen

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,9 +1,22 @@
-function App() {
-  return (
-    <div>
-      APP
-    </div>
-  );
-}
+import React from "react";
 
-export default App;
+export class App extends React.Component {
+  state = { lat: null };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ ...this.state, lat: position.coords.latitude });
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+      </div>
+    );
+  }
+}
