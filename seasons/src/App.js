3cd3abbf790599e313e9081d496811d9663c4ef4
Shane O'Moore <shaneiadt@gmail.com>
2021-10-15 15:16:31 +0100

    init state inside constructor

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export class App extends React.Component {
-  state = { latitude: null };
-
   // eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = {
+      latitude: null
+    }
   }
 
   componentDidMount() {
