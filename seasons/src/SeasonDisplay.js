ccc37cdc655555e66b46ac144ddc5ee0ba2c9a03
Shane O'Moore <shaneiadt@gmail.com>
2021-10-16 12:15:36 +0100

    determine season from lat and month

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+
+    }
+
+    return lat > 0 ? 'winter' : 'summer';
+}
+
 export const SeasonDisplay = ({ lat }) => {
+    const month = getSeason(lat, new Date().getMonth());
+
     return (
         <div>
-            Latitude: {lat}
+            Month: {month}
         </div>
     )
 }
