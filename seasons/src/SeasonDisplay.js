ae603a689235cceb69f7ddad4187db6522cb93cb
Shane O'Moore <shaneiadt@gmail.com>
2021-10-18 10:34:21 +0100

    use config object

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const config = {
+    summer: {
+        text: "Let's hit the beach",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, it's cold",
+        iconName: "snowflake"
+    }
+}
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -11,13 +22,11 @@ const getSeason = (lat, month) => {
 
 export const SeasonDisplay = ({ lat }) => {
     const season = getSeason(lat, new Date().getMonth());
-    const getText = () => season === 'winter' ? <h1>Burr, it's chilly</h1> : <h1>Let's hit the beach</h1>
-    const getIcon = () => season === 'winter' ? <i className="snowflake icon" /> : <i className="sun icon" />
 
     return (
         <div>
-            {getIcon()}
-            {getText()}
+            <i className={`${config[season].iconName} icon`} />
+            <h1>{config[season].text}</h1>
         </div>
     )
 }
