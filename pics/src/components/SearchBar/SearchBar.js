0369f661fb3cd7ab13870916232af542db6dd837
Shane O'Moore <shaneiadt@gmail.com>
2021-10-19 12:12:35 +0100

    setup controlled input field

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Form, Input } from "semantic-ui-react";
 
 export class SearchBar extends React.Component {
+    state = {
+        term: ""
+    };
 
-    onInputChange(_, { value }) {
-        console.log(value);
+    onInputChange = (_, { value }) => {
+        this.setState({ term: value });
     }
 
     render() {
         return (
             <Form>
                 <Form.Field>
-                    <Input placeholder='Image Search' onChange={this.onInputChange} />
+                    <Input placeholder='Image Search' value={this.state.term} onChange={this.onInputChange} />
                 </Form.Field>
             </Form>
         );
