679d34a401b654982c98b70ed74befdc31f91792
Shane O'Moore <shaneiadt@gmail.com>
2021-10-19 12:16:52 +0100

    add loading state

--- pics/src/components/SearchBar/SearchBar.js
+++ pics/src/components/SearchBar/SearchBar.js
@@ -3,16 +3,17 @@ import { Form, Input } from "semantic-ui-react";
 
 export class SearchBar extends React.Component {
     state = {
+        loading: false,
         term: ""
     };
 
     onInputChange = (_, { value }) => {
-        this.setState({ term: value });
+        this.setState({ ...this.state, term: value });
     }
 
     render() {
         return (
-            <Form>
+            <Form loading={this.state.loading}>
                 <Form.Field>
                     <Input placeholder='Image Search' value={this.state.term} onChange={this.onInputChange} />
                 </Form.Field>
