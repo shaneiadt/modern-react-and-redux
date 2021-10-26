7ba81e4145581381e2cc82548ecbba46343ed029
Shane O'Moore <shaneiadt@gmail.com>
2021-10-26 11:58:42 +0100

    make app a class component

--- pics/src/components/App/App.js
+++ pics/src/components/App/App.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import { SearchBar } from '../SearchBar/SearchBar';
 
-function App() {
-    const onSearchSubmit = (term) => {
+class App extends React.Component {
+    onSearchSubmit = (term) => {
         console.log(term);
     }
 
-    return (
-        <Container style={{padding:'20px', margin: '20px', border:'1px solid #f1f1f1', borderRadius: '5px'}}>
-            <SearchBar onSearchSubmit={onSearchSubmit} />
-        </Container>
-    );
+    render() {
+        return (
+            <Container style={{ padding: '20px', margin: '20px', border: '1px solid #f1f1f1', borderRadius: '5px' }}>
+                <SearchBar onSearchSubmit={this.onSearchSubmit} />
+            </Container>
+        );
+    }
+
 }
 
 export default App;
