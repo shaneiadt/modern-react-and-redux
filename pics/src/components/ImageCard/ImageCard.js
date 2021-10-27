ca8f6554feed2f1cdbde89026bb3845baedcabd7
Shane O'Moore <shaneiadt@gmail.com>
2021-10-27 15:19:02 +0100

    dynamicially set grid row spans

--- pics/src/components/ImageCard/ImageCard.js
+++ pics/src/components/ImageCard/ImageCard.js
@@ -4,19 +4,24 @@ export class ImageCard extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = { spans: 0 };
+
         this.imgRef = React.createRef();
     }
 
-    onLoad = () => {
-        console.log(this.imgRef.current.clientHeight);
+    setSpans = () => {
+        const height = this.imgRef.current.clientHeight;
+        const spans = Math.ceil(height / 10 + 1);
+
+        this.setState({ spans });
     }
 
     render() {
         const { description, urls: { regular } } = this.props.image;
 
         return (
-            <div>
-                <img ref={this.imgRef} alt={description} src={regular} onLoad={this.onLoad} />
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+                <img ref={this.imgRef} alt={description} src={regular} onLoad={this.setSpans} />
             </div>
         )
     }
--- pics/src/components/ImageList/ImageList.js
+++ pics/src/components/ImageList/ImageList.js
@@ -4,8 +4,6 @@ import { ImageCard } from "../ImageCard/ImageCard";
 import './ImageList.css';
 
 export const ImageList = (props) => {
-    console.log(props.images);
-
     const images = props.images.map((image) => <ImageCard image={image} key={image.id} />);
 
     return (
