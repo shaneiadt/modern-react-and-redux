fc16bce336d13d0d75be585e2d3e58b5d4ec4f1f
Shane O'Moore <shaneiadt@gmail.com>
2021-10-27 10:11:31 +0100

    add key prop for list rendering

--- pics/src/components/ImageList/ImageList.js
+++ pics/src/components/ImageList/ImageList.js
@@ -3,11 +3,7 @@ import React from "react";
 export const ImageList = (props) => {
     console.log(props.images);
 
-    const images = props.images.map((image, i) => {
-        return (
-            <img alt="some random" src={image.urls.regular} />
-        );
-    });
+    const images = props.images.map(image => <img key={image.id} alt={image.alt_description} src={image.urls.regular} />);
 
     return (
         <>
