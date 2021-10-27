2c1781345e4a7a3405d1691596a74669c298f476
Shane O'Moore <shaneiadt@gmail.com>
2021-10-27 10:13:07 +0100

    destructure props to map

--- pics/src/components/ImageList/ImageList.js
+++ pics/src/components/ImageList/ImageList.js
@@ -3,7 +3,7 @@ import React from "react";
 export const ImageList = (props) => {
     console.log(props.images);
 
-    const images = props.images.map(image => <img key={image.id} alt={image.alt_description} src={image.urls.regular} />);
+    const images = props.images.map(({ id, description, urls: { regular } }) => <img key={id} alt={description} src={regular} />);
 
     return (
         <>
