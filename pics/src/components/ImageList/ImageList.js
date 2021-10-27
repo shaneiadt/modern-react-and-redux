41a03aeedf13e90c8fefa6d5355c28ef32c24910
Shane O'Moore <shaneiadt@gmail.com>
2021-10-27 14:36:39 +0100

    use ImageCard inside ImageList

--- pics/src/components/ImageList/ImageList.js
+++ pics/src/components/ImageList/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+import { ImageCard } from "../ImageCard/ImageCard";
 import './ImageList.css';
 
 export const ImageList = (props) => {
     console.log(props.images);
 
-    const images = props.images.map(({ id, description, urls: { regular } }) => <img key={id} alt={description} src={regular} />);
+    const images = props.images.map((image) => <ImageCard image={image} key={image.id} />);
 
     return (
         <div className="image-list">
