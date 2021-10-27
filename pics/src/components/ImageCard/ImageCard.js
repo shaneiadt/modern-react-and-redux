40cfea2422b3b9250a1034cc837ad0047e61cad7
Shane O'Moore <shaneiadt@gmail.com>
2021-10-27 14:41:52 +0100

    create image ref

--- pics/src/components/ImageCard/ImageCard.js
+++ pics/src/components/ImageCard/ImageCard.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imgRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imgRef);
+    }
+
     render() {
         const { description, urls: { regular } } = this.props.image;
 
         return (
             <div>
-                <img alt={description} src={regular} />
+                <img ref={this.imgRef} alt={description} src={regular} />
             </div>
         )
     }
