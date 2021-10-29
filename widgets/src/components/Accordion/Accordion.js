db8344a76d0c89353b948a156f78bc0dea237f62
Shane O'Moore <shaneiadt@gmail.com>
2021-10-29 11:47:30 +0100

    pass some items to Accordion component

--- widgets/src/components/Accordion/Accordion.js
+++ widgets/src/components/Accordion/Accordion.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export const Accordion = () => {
+export const Accordion = ({ items }) => {
+    console.log({ items });
     return (
         <div>Accordion</div>
     );
--- widgets/src/components/App/App.js
+++ widgets/src/components/App/App.js
@@ -2,10 +2,25 @@ import React from "react";
 
 import { Accordion } from '../Accordion/Accordion';
 
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a frontend JS framework'
+  },
+  {
+    title: 'What use React?',
+    content: 'React is super popular'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+];
+
 function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
   );
 }
