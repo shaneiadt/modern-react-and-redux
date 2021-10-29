9d5c394a6ba398122a13d126e77e311bdae4e272
Shane O'Moore <shaneiadt@gmail.com>
2021-10-29 12:01:07 +0100

    render item props in accordion

--- widgets/src/components/Accordion/Accordion.js
+++ widgets/src/components/Accordion/Accordion.js
@@ -10,59 +10,25 @@ export const AccordionComponent = ({ items }) => {
 
     return (
         <Accordion fluid styled>
-            <Accordion.Title
-                active={activeIndex === 0}
-                index={0}
-                onClick={() => handleClick(0)}
-            >
-                <Icon name='dropdown' />
-                What is a dog?
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 0}>
-                <p>
-                    A dog is a type of domesticated animal. Known for its loyalty and
-                    faithfulness, it can be found as a welcome guest in many households
-                    across the world.
-                </p>
-            </Accordion.Content>
-
-            <Accordion.Title
-                active={activeIndex === 1}
-                index={1}
-                onClick={() => handleClick(1)}
-            >
-                <Icon name='dropdown' />
-                What kinds of dogs are there?
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 1}>
-                <p>
-                    There are many breeds of dogs. Each breed varies in size and
-                    temperament. Owners often select a breed of dog that they find to be
-                    compatible with their own lifestyle and desires from a companion.
-                </p>
-            </Accordion.Content>
-
-            <Accordion.Title
-                active={activeIndex === 2}
-                index={2}
-                onClick={() => handleClick(2)}
-            >
-                <Icon name='dropdown' />
-                How do you acquire a dog?
-            </Accordion.Title>
-            <Accordion.Content active={activeIndex === 2}>
-                <p>
-                    Three common ways for a prospective owner to acquire a dog is from
-                    pet shops, private owners, or shelters.
-                </p>
-                <p>
-                    A pet shop may be the most convenient way to buy a dog. Buying a dog
-                    from a private owner allows you to assess the pedigree and
-                    upbringing of your dog before choosing to take it home. Lastly,
-                    finding your dog from a shelter, helps give a good home to a dog who
-                    may not find one so readily.
-                </p>
-            </Accordion.Content>
+            {items.map((item, index) => (
+                <div
+                    key={`accordion-item-${index}`}
+                >
+                    <Accordion.Title
+                        active={activeIndex === index}
+                        index={index}
+                        onClick={() => handleClick(index)}
+                    >
+                        <Icon name='dropdown' />
+                        {item.title}
+                    </Accordion.Title>
+                    <Accordion.Content active={activeIndex === index}>
+                        <p>
+                            {item.content}
+                        </p>
+                    </Accordion.Content>
+                </div>
+            ))}
         </Accordion>
     );
 }
