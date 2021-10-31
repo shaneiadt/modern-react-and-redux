a03a34cc4786a697e50bd01f89cc482e3b471215
Shane O'Moore <shaneiadt@gmail.com>
2021-10-31 15:12:48 +0000

    custom results renderer

--- widgets/src/components/Search/Search.js
+++ widgets/src/components/Search/Search.js
@@ -3,7 +3,7 @@ import { Search as Searchbox } from "semantic-ui-react";
 import axios from 'axios';
 
 export const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
     const [results, setResults] = useState([]);
 
     useEffect(() => {
@@ -17,14 +17,21 @@ export const Search = () => {
                     srsearch: term
                 }
             });
-            
-            setResults(data.query.search);
+
+            setResults(data.query.search.map(({ title, snippet }) => ({ title, description: snippet })));
         };
 
-        if(term) search();
+        if (term) search();
     }, [term]);
+
+    const resultsRenderer = ({ title, description }) => (
+        <>
+            <h4>{title}</h4>
+            <div dangerouslySetInnerHTML={{ __html: description }} />
+        </>
+    );
 
     const onSearchChange = (_, { value }) => setTerm(value);
 
-    return <Searchbox fluid value={term} results={results} onSearchChange={onSearchChange} />;
+    return <Searchbox fluid value={term} results={results} onSearchChange={onSearchChange} resultRenderer={resultsRenderer} />;
 }
