ad0b0425f58faf510de1cd8e0365c366417ad1a2
Shane O'Moore <shaneiadt@gmail.com>
2021-10-31 15:05:36 +0000

    search wikipedia api

--- widgets/src/components/Search/Search.js
+++ widgets/src/components/Search/Search.js
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Search as Searchbox } from "semantic-ui-react";
+import axios from 'axios';
 
 export const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        console.log('TERM', term);
+        const search = async () => {
+            const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: term
+                }
+            });
+            
+            setResults(data.query.search);
+        };
+
+        if(term) search();
     }, [term]);
 
     const onSearchChange = (_, { value }) => setTerm(value);
 
-    return <Searchbox fluid value={term} onSearchChange={onSearchChange} />;
+    return <Searchbox fluid value={term} results={results} onSearchChange={onSearchChange} />;
 }
