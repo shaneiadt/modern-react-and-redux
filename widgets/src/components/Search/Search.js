45f0f4e68715405b01a62c6e7b5c932da660e5a7
Shane O'Moore <shaneiadt@gmail.com>
2021-10-31 14:47:31 +0000

    useEffect hook

--- widgets/src/components/Search/Search.js
+++ widgets/src/components/Search/Search.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Search as Searchbox } from "semantic-ui-react";
 
 export const Search = () => {
     const [term, setTerm] = useState('');
 
-    const onSearchChange = (e, { value }) => setTerm(value);
+    useEffect(() => {
+        console.log('TERM', term);
+    }, [term]);
+
+    const onSearchChange = (_, { value }) => setTerm(value);
 
     return <Searchbox fluid value={term} onSearchChange={onSearchChange} />;
 }
