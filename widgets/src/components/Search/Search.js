350eb64e16552c9d51818dc18341fc7d3e537248
Shane O'Moore <shaneiadt@gmail.com>
2021-11-01 08:37:28 +0000

    use timeout to debounce the search input

--- widgets/src/components/Search/Search.js
+++ widgets/src/components/Search/Search.js
@@ -2,48 +2,35 @@ import React, { useEffect, useState } from "react";
 import { Search as Searchbox, Icon } from "semantic-ui-react";
 import axios from 'axios';
 
-function useDebounce(value, delay) {
-    // State and setters for debounced value
-    const [debouncedValue, setDebouncedValue] = useState(value);
-    useEffect(
-        () => {
-            // Update debounced value after delay
-            const handler = setTimeout(() => {
-                setDebouncedValue(value);
-            }, delay);
-            // Cancel the timeout if value changes (also on delay change or unmount)
-            // This is how we prevent debounced value from updating if value is changed ...
-            // .. within the delay period. Timeout gets cleared and restarted.
-            return () => {
-                clearTimeout(handler);
-            };
-        },
-        [value, delay] // Only re-call effect if value or delay changes
-    );
-    return debouncedValue;
-}
+import './Search.css';
 
 export const Search = () => {
-    const [term, setTerm] = useState('programming');
+    const [term, setTerm] = useState('');
+    const [loading, setLoading] = useState(false);
     const [results, setResults] = useState([]);
-    const debouncedSearchTerm = useDebounce(search, 500);
-
-    const search = async () => {
-        const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
-            params: {
-                action: "query",
-                list: "search",
-                origin: "*",
-                format: "json",
-                srsearch: term
-            }
-        });
-
-        setResults(data.query.search.map(({ pageid, title, snippet }) => ({ pageid, title, description: snippet })));
-    };
 
     useEffect(() => {
-        if (term) debouncedSearchTerm();
+        const search = async () => {
+            setLoading(true);
+            const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: term
+                }
+            });
+
+            setResults(data.query.search.map(({ pageid, title, snippet }) => ({ pageid, title, description: snippet })));
+            setLoading(false);
+        };
+
+        const timeoutId = setTimeout(() => {
+            if (term) search();
+        }, 500);
+
+        return () => clearTimeout(timeoutId);
     }, [term]);
 
     const resultsRenderer = ({ pageid, title, description }) => (
@@ -55,5 +42,5 @@ export const Search = () => {
 
     const onSearchChange = (_, { value }) => setTerm(value);
 
-    return <Searchbox fluid value={term} results={results} onSearchChange={onSearchChange} resultRenderer={resultsRenderer} />;
+    return <Searchbox fluid value={term} loading={loading} results={results} onSearchChange={onSearchChange} resultRenderer={resultsRenderer} />;
 }
