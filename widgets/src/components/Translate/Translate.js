1db62262f4fbacc2a094dda15fccf80e38fc62fd
Shane O'Moore <shaneiadt@gmail.com>
2021-11-05 11:42:01 +0000

    state for additional input box

--- widgets/src/components/Translate/Translate.js
+++ widgets/src/components/Translate/Translate.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Input } from 'semantic-ui-react';
 import { Dropdown } from '../Dropdown/Dropdown';
 
 const options = [
@@ -9,10 +10,15 @@ const options = [
 
 export const Translate = () => {
     const [lang, setLang] = useState(options[0]);
+    const [text, setText] = useState("");
 
     const onSelectedChange = (e, { value }) => setLang(options.find(option => option.value === value));
 
     return (
-        <Dropdown selection={lang.value} onSelectedChange={onSelectedChange} options={options.map(({ value, label }) => ({ key: value, text: label, value }))} />
+        <>
+            <p><Input onChange={(e, { value }) => setText(value)} value={text} placeholder="Enter text to translate..." fluid /></p>
+            <p><Dropdown selection={lang.value} onSelectedChange={onSelectedChange} options={options.map(({ value, label }) => ({ key: value, text: label, value }))} /></p>
+            <p>...</p>
+        </>
     );
 }
