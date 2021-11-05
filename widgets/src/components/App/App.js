c636ccd8f5755a53d40ed61a9e080ddd5d6e39d2
Shane O'Moore <shaneiadt@gmail.com>
2021-11-05 11:34:11 +0000

    remove unused stuff

--- widgets/src/components/App/App.js
+++ widgets/src/components/App/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
-// import { AccordionComponent } from '../Accordion/Accordion';
-// import { Search } from "../Search/Search";
-// import { Dropdown } from '../Dropdown/Dropdown';
-
 import { Translate } from "../Translate/Translate";
 
 // const items = [
@@ -22,18 +18,9 @@ import { Translate } from "../Translate/Translate";
 //   }
 // ];
 
-// const options = [
-//   { key: 1, text: 'One', value: 1 },
-//   { key: 2, text: 'Two', value: 2 },
-//   { key: 3, text: 'Three', value: 3 },
-// ]
-
 function App() {
   return (
     <Container>
-      {/* <AccordionComponent items={items} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown options={options} /> */}
       <Translate />
     </Container>
   );
