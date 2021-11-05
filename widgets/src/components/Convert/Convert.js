9590fc5b9d533a4ba606491939ef692e3b31e63b
Shane O'Moore <shaneiadt@gmail.com>
2021-11-05 14:17:38 +0000

    make api request

--- widgets/src/components/Convert/Convert.js
+++ widgets/src/components/Convert/Convert.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
-
-// process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY
+import axios from 'axios';
 
 export const Convert = ({ lang, text }) => {
     useEffect(() => {
-
+        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+            params: {
+                q: text,
+                target: lang,
+                key: process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY
+            }
+        });
     }, [lang, text]);
 
     return (
