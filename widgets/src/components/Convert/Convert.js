92ae1cb73b2ddfffa46647d59ab62979bbf3f4cb
Shane O'Moore <shaneiadt@gmail.com>
2021-11-05 14:22:45 +0000

    display translated piece of text

--- widgets/src/components/Convert/Convert.js
+++ widgets/src/components/Convert/Convert.js
@@ -2,19 +2,27 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 export const Convert = ({ lang, text }) => {
+    const [translated, setTranslated] = useState("");
+
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params: {
-                q: text,
-                target: lang,
-                key: process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY
-            }
-        });
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: lang,
+                    key: process.env.REACT_APP_GOOGLE_TRANSLATE_API_KEY
+                }
+            });
+
+            setTranslated(data.data.translations[0].translatedText);
+        }
+
+        doTranslation(text);
     }, [lang, text]);
 
     return (
         <div>
-            Convert
+            <h1>{translated}</h1>
         </div>
     )
 }
