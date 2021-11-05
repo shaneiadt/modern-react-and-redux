1c8d513715d75bde7ed26a11a5f69a8ed9f89fd6
Shane O'Moore <shaneiadt@gmail.com>
2021-11-05 11:16:28 +0000

    lang state

--- widgets/src/components/Translate/Translate.js
+++ widgets/src/components/Translate/Translate.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Translate = ({ options, lang }) => {
+    const [lang, setLang] = useState("en");
+    
     return (
         <div>
             Translate
