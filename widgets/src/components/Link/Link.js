0a873b0887c912a075cc667c6f756e4ece297230
Shane O'Moore <shaneiadt@gmail.com>
2021-11-07 13:55:37 +0000

    update window history

--- widgets/src/components/Link/Link.js
+++ widgets/src/components/Link/Link.js
@@ -3,6 +3,8 @@ import React from "react";
 export const Link = ({ className, href, children }) => {
     const onClick = (e) => {
         e.preventDefault();
+
+        window.history.pushState({}, '', href);
     }
 
     return <a onClick={onClick} href={href} className={className}>{children}</a>;
