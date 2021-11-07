b6409ac51ae08bd89009449132c007cb987aa3d1
Shane O'Moore <shaneiadt@gmail.com>
2021-11-07 13:57:38 +0000

    emit popstate

--- widgets/src/components/Link/Link.js
+++ widgets/src/components/Link/Link.js
@@ -5,6 +5,10 @@ export const Link = ({ className, href, children }) => {
         e.preventDefault();
 
         window.history.pushState({}, '', href);
+
+        const navEvent = new PopStateEvent('popstate');
+
+        window.dispatchEvent(navEvent);
     }
 
     return <a onClick={onClick} href={href} className={className}>{children}</a>;
