6336a70d587749d75fd8def4c8e59c3353e9183b
Shane O'Moore <shaneiadt@gmail.com>
2021-11-11 14:47:10 +0000

    songs reducer

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,13 @@
+export const songsReducer = () => {
+    return [
+        {
+            title: 'No Diggity', duration: '4:00'
+        },
+        {
+            title: 'All Start', duration: '2:17'
+        },
+        {
+            title: 'I Want It That Way', duration: '1:45'
+        }
+    ];
+}
