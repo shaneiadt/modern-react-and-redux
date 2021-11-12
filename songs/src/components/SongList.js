a24acabec36bb7526906fec60b16866a7c894a56
Shane O'Moore <shaneiadt@gmail.com>
2021-11-12 10:31:58 +0000

    pass duration prop into selectedSong state as well

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -13,19 +13,20 @@ class SongList extends Component {
                 <Grid>
                     <Grid.Row columns={2}>
                         <Grid.Column>
-                            {songs.map(({ title, duration }) =>
-                                <Item key={title} style={{ border: '1px solid #f1f1f1', padding: '10px', margin: '10px 0' }}>
+                            {songs.map((song) =>
+                                <Item key={song.title} style={{ border: '1px solid #f1f1f1', padding: '10px', margin: '10px 0' }}>
                                     <Item.Content style={{ display: 'flex', alignItems: 'center' }}>
-                                        <Item.Header><strong>{title}</strong> - {duration}</Item.Header>
+                                        <Item.Header><strong>{song.title}</strong> - {song.duration}</Item.Header>
                                         <Item.Extra style={{ flexGrow: '1', textAlign: 'right' }}>
-                                            <Button onClick={() => this.props.selectSong(title)}>Select</Button>
+                                            <Button onClick={() => this.props.selectSong(song)}>Select</Button>
                                         </Item.Extra>
                                     </Item.Content>
                                 </Item>)}
                         </Grid.Column>
                         <Grid.Column>
-                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-                                <h2>{selectedSong ?? 'No Song Selected'}</h2>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', flexDirection: 'column' }}>
+                                <h2>{selectedSong ? `${selectedSong.title}` : 'No Song Selected'}</h2>
+                                <h3>{selectedSong && `${selectedSong.duration}`}</h3>
                             </div>
                         </Grid.Column>
                     </Grid.Row>
