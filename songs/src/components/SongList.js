7663a3d30e84cecc8d67535940fa241a1c2185c5
Shane O'Moore <shaneiadt@gmail.com>
2021-11-12 10:07:29 +0000

    use Item from semantic ui react

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 
+import { Button, Item, Container } from 'semantic-ui-react';
+
 class SongList extends Component {
     render() {
         const { songs } = this.props;
 
         return (
-            <div>
-                {songs.map(({ title, duration }) => <p key={title}>{title} - {duration}</p>)}
-            </div>
+            <Container>
+                {songs.map(({ title, duration }) =>
+                    <Item key={title} style={{ border: '1px solid #f1f1f1', padding: '10px', margin: '10px 0' }}>
+                        <Item.Content style={{ display: 'flex', alignItems: 'center' }}>
+                            <Item.Header>{title}</Item.Header>
+                            <Item.Description>{duration}</Item.Description>
+                            <Item.Extra style={{ flexGrow: '1', textAlign: 'right' }}>
+                                <Button>Select</Button>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>)}
+            </Container>
         );
     }
 }
