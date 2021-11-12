6c2f0ea4f1c24f81d3881076c56e7317d1b2f7c4
Shane O'Moore <shaneiadt@gmail.com>
2021-11-12 10:14:38 +0000

    use Grid to split content up a bit

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 
-import { Button, Item, Container } from 'semantic-ui-react';
+import { Button, Item, Container, Grid } from 'semantic-ui-react';
 
 class SongList extends Component {
     render() {
@@ -9,15 +9,26 @@ class SongList extends Component {
 
         return (
             <Container>
-                {songs.map(({ title, duration }) =>
-                    <Item key={title} style={{ border: '1px solid #f1f1f1', padding: '10px', margin: '10px 0' }}>
-                        <Item.Content style={{ display: 'flex', alignItems: 'center' }}>
-                            <Item.Header><strong>{title}</strong> - {duration}</Item.Header>
-                            <Item.Extra style={{ flexGrow: '1', textAlign: 'right' }}>
-                                <Button>Select</Button>
-                            </Item.Extra>
-                        </Item.Content>
-                    </Item>)}
+                <Grid>
+                    <Grid.Row columns={2}>
+                        <Grid.Column>
+                            {songs.map(({ title, duration }) =>
+                                <Item key={title} style={{ border: '1px solid #f1f1f1', padding: '10px', margin: '10px 0' }}>
+                                    <Item.Content style={{ display: 'flex', alignItems: 'center' }}>
+                                        <Item.Header><strong>{title}</strong> - {duration}</Item.Header>
+                                        <Item.Extra style={{ flexGrow: '1', textAlign: 'right' }}>
+                                            <Button>Select</Button>
+                                        </Item.Extra>
+                                    </Item.Content>
+                                </Item>)}
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                                <h2>SELECTED SONG</h2>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Container>
         );
     }
