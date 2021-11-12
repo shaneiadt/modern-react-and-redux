b7c44a6bbc8f48f0a7a2fee118f558a9dbccc52c
Shane O'Moore <shaneiadt@gmail.com>
2021-11-12 09:37:28 +0000

    SongList component

---
+++ songs/src/components/SingList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+export default class SongList extends Component {
+    render() {
+        return (
+            <div>
+                SONG LIST
+            </div>
+        );
+    }
+}
