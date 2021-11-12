ae0ea0d8a176041736b0ac475d9f342e61c40d88
Shane O'Moore <shaneiadt@gmail.com>
2021-11-12 09:45:17 +0000

    mapStateToProps in SongList

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -11,4 +11,10 @@ class SongList extends Component {
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    console.log(state);
+
+    return state;
+}
+
+export default connect(mapStateToProps)(SongList);
