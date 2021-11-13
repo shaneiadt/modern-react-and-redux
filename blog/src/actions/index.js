dd5d1f50c20a615307dcebb46a4e276424e552da
Shane O'Moore <shaneiadt@gmail.com>
2021-11-13 12:12:18 +0000

    fetch action creator and wire up PostList to redux

---
+++ blog/src/actions/index.js
@@ -0,0 +1,11 @@
+export const componentDidMount = () => {
+    return {
+        type: 'MOUNTED'
+    }
+}
+
+export const fetchPosts = () => {
+    return {
+        type: 'FETCH_POSTS'
+    }
+}
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Post List</div>
@@ -8,4 +15,10 @@ class PostList extends Component {
     }
 }
 
-export default PostList;
+const mapStateToProps = (state) => {
+    return {
+        posts: []
+    }
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
