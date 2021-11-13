3f5150a7be19a7d15b25469a48b270612b74019a
Shane O'Moore <shaneiadt@gmail.com>
2021-11-13 10:54:19 +0000

    re-structure

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import App from './App';
+import App from './components/App';
 import reducers from './reducers';
 
 ReactDOM.render(
