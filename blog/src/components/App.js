800ec2481a4b51a9798abc0ed93bece34147e0ce
Shane O'Moore <shaneiadt@gmail.com>
2021-11-13 10:59:39 +0000

    PostList component

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container } from 'semantic-ui-react';
+import PostList from "./PostList";
 
 const App = () => {
   return (
     <Container>
-      blog app
+      <PostList />
     </Container>
   );
 }
