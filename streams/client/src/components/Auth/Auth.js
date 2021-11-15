2cd107c314b85dc810710cdfcfa8bb861041cb41
Shane O'Moore <shaneiadt@gmail.com>
2021-11-15 16:25:34 +0000

    OAuth Google flow

--- streams/client/src/components/Auth/Auth.js
+++ streams/client/src/components/Auth/Auth.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Auth extends Component {
+    state = {
+        isSignedIn: null
+    };
+
     componentDidMount() {
         try {
             window.gapi.load('client:auth2', () => {
@@ -10,7 +14,9 @@ class Auth extends Component {
                         scope: 'email'
                     })
                     .then(() => {
-                        console.log('DONE');
+                        this.auth = window.gapi.auth2.getAuthInstance();
+
+                        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
                     });
             });
         } catch (error) {
@@ -18,10 +24,14 @@ class Auth extends Component {
         }
     }
 
+    renderAuthButton = () => {
+        if (this.state.isSignedIn === null) return <div>I don't know if we are signed in!</div>;
+        else if (this.state.isSignedIn) return <div>I am signed in :)</div>;
+        else return <div>I am not signed in :(</div>;
+    }
+
     render() {
-        return (
-            <div>Auth</div>
-        );
+        return this.renderAuthButton();
     }
 }
 
