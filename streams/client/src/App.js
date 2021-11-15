0d206d4c5dfcac1796c72fe66d793b649ded2c6f
Shane O'Moore <shaneiadt@gmail.com>
2021-11-15 14:47:42 +0000

    testing routing

--- streams/client/src/App.js
+++ streams/client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      Streams App
-    </div>
+    <BrowserRouter>
+      <Route path="/" render={() => <p>Page 1</p>} />
+      <Route path="/2" render={() => <p>Page 2</p>} />
+    </BrowserRouter>
   );
 }
 
