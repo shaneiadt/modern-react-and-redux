bf6f7e5d090cd8559e1b6856dc5d958285bede84
Shane O'Moore <shaneiadt@gmail.com>
2021-11-15 10:19:00 +0000

    fetchUser action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,12 +1,21 @@
-import posts from '../apis/jsonPlaceholder';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
     return async (dispatch, getState) => {
-        const response = await posts.get('/posts');
+        const response = await jsonPlaceholder.get('/posts');
 
         dispatch({
             type: 'FETCH_POSTS',
             payload: response
         });
     }
+}
+
+export const fetchUser = (userId) => async (dispatch, getState) => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response
+    });
 }
