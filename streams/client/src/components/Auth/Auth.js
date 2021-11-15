549f7da26f6de760b2d2281dc53c07bc0a926d65
Shane O'Moore <shaneiadt@gmail.com>
2021-11-15 16:18:31 +0000

    fix gapi client ref

--- streams/client/src/components/Auth/Auth.js
+++ streams/client/src/components/Auth/Auth.js
@@ -4,10 +4,14 @@ class Auth extends Component {
     componentDidMount() {
         try {
             window.gapi.load('client:auth2', () => {
-                window.gapi.init({
-                    clientId: '224043226631-qehv8ophoeo34ba844mhcdbep4280sp0.apps.googleusercontent.com',
-                    scope: 'email'
-                });
+                window.gapi.client
+                    .init({
+                        clientId: '224043226631-qehv8ophoeo34ba844mhcdbep4280sp0.apps.googleusercontent.com',
+                        scope: 'email'
+                    })
+                    .then(() => {
+                        console.log('DONE');
+                    });
             });
         } catch (error) {
             console.error(error);
