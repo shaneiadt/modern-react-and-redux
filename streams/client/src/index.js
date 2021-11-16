4a3aa9a3fe1d60849d17b45fcc3b1789860063ed
Shane O'Moore <shaneiadt@gmail.com>
2021-11-16 11:45:03 +0000

    enable redux dev tools extension

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -10,7 +10,7 @@ import reducers from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
       <App />
     </Provider>
   </React.StrictMode>,
