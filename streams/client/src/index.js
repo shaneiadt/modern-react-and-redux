942929e5b568646c748584c4bfb688f645ae7769
Shane O'Moore <shaneiadt@gmail.com>
2021-11-16 11:14:23 +0000

    setup redux and dummy reducer

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -2,11 +2,17 @@ import 'semantic-ui-css/semantic.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducers from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={createStore(reducers)}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
