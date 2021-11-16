cf0e47ac741248112cbe301ca3282a287fba3ae4
Shane O'Moore <shaneiadt@gmail.com>
2021-11-16 11:56:40 +0000

    redux dev tools compose enhancers setup

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -3,14 +3,16 @@ import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './App';
 import reducers from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <Provider store={createStore(reducers, composeEnhancers(applyMiddleware()))}>
       <App />
     </Provider>
   </React.StrictMode>,
