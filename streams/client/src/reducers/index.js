ad961c736cfe064402e2ca107123ab764147d8eb
Shane O'Moore <shaneiadt@gmail.com>
2021-11-16 11:25:47 +0000

    authReducer

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,5 +1,20 @@
 import { combineReducers } from 'redux';
 
+const INITIAL_STATE = {
+    isSignedIn: null
+}
+
+const authReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case 'SIGN_IN':
+            return { ...state, isSignedIn: true };
+        case 'SIGN_OUT':
+            return { ...state, isSignedIn: false };
+        default:
+            return state;
+    }
+}
+
 export default combineReducers({
-    replaceMe: () => 10
+    auth: authReducer
 });
