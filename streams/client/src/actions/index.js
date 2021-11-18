cc813c6a724860e82b760a35582c3cda0703f6e6
Shane O'Moore <shaneiadt@gmail.com>
2021-11-18 14:04:32 +0000

    RESTful stream action creators

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -22,3 +22,39 @@ export const createStream = formValues => async dispatch => {
         payload: response.data
     });
 }
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get(`/streams`);
+
+    dispatch({
+        type: ACTIONS.FETCH_STREAMS,
+        payload: response.data
+    });
+}
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({
+        type: ACTIONS.FETCH_STREAM,
+        payload: response.data
+    });
+}
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({
+        type: ACTIONS.EDIT_STREAM,
+        payload: response.data
+    });
+}
+
+export const deleteStream = id => async dispatch => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({
+        type: ACTIONS.DELETE_STREAM,
+        payload: id
+    });
+}
