e296ec89e23c1b204ac4a71ed5b3e17718b25d9e
Shane O'Moore <shaneiadt@gmail.com>
2021-11-19 11:06:48 +0000

    update stream list styles

--- streams/client/src/components/Streams/StreamList.js
+++ streams/client/src/components/Streams/StreamList.js
@@ -18,17 +18,17 @@ class StreamList extends Component {
                 <h2>Streams</h2>
                 <List divided style={{ border: '1px solid #f1f1f1', padding: '10px', borderRadius: '3px' }}>
                     {streams.map(({ id, title, description, userId }) => (
-                        <List.Item key={id}>
-                            <List.Content style={{ margin: '10px 0' }}>
+                        <List.Item key={id} style={{ margin: '10px 0', display: 'flex', flexDirection: 'row', padding: '20px' }}>
+                            <List.Content style={{ flexGrow: '1' }}>
                                 <List.Header as='a'>{title}</List.Header>
                                 <List.Description as='a'>{description}</List.Description>
-                                {userId === this.props.currentUserId && (
-                                    <div style={{ marginTop: '5px', textAlign: 'right' }}>
-                                        <Button>Edit</Button>
-                                        <Button>Delete</Button>
-                                    </div>
-                                )}
                             </List.Content>
+                            {userId === this.props.currentUserId && (
+                                <div>
+                                    <Button>Edit</Button>
+                                    <Button color='red'>Delete</Button>
+                                </div>
+                            )}
                         </List.Item>
                     ))}
                 </List >
