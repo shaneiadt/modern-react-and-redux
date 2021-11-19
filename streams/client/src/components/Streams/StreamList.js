34a67ef48f977d6c96a8529db40bbd47ec498570
Shane O'Moore <shaneiadt@gmail.com>
2021-11-19 10:01:06 +0000

    render streams

--- streams/client/src/components/Streams/StreamList.js
+++ streams/client/src/components/Streams/StreamList.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { values } from 'lodash';
+import { List } from 'semantic-ui-react';
 
 import { fetchStreams } from '../../actions';
 
@@ -13,7 +14,16 @@ class StreamList extends Component {
         const streams = values(this.props.streams);
 
         return (
-            <div> StreamList - {streams.length}</div >
+            <List divided>
+                {streams.map(({ id, title, description }) => (
+                    <List.Item key={id}>
+                        <List.Content>
+                            <List.Header as='a'>{title}</List.Header>
+                            <List.Description as='a'>{description}</List.Description>
+                        </List.Content>
+                    </List.Item>
+                ))}
+            </List >
         );
     }
 }
