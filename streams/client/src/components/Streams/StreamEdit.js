af0a43d0b66d91b7d4914b9c7483e325a0a083a9
Shane O'Moore <shaneiadt@gmail.com>
2021-11-19 11:49:10 +0000

    fetch stream to edit

--- streams/client/src/components/Streams/StreamEdit.js
+++ streams/client/src/components/Streams/StreamEdit.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from 'react-redux';
 
-export const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+import { fetchStream } from '../../actions';
+
+class StreamEdit extends Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        console.log(this.props.stream);
+        return (
+            <div>StreamEdit</div>
+        );
+    }
 }
+
+const mapStateToProps = (state, ownProps) => {
+    const stream = state.streams[ownProps.match.params.id];
+
+    return {
+        stream
+    }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
--- streams/client/src/components/Streams/index.js
+++ streams/client/src/components/Streams/index.js
@@ -1,5 +1,5 @@
 export { StreamDelete } from './StreamDelete';
-export { StreamEdit } from './StreamEdit';
+export { default as StreamEdit } from './StreamEdit';
 export { default as StreamList } from './StreamList';
 export { StreamShow } from './StreamShow';
 export { default as StreamCreate } from './StreamCreate';
