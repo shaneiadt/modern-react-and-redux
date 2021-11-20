40c021abb41f1f0c9c01b83ce7619970dde6d1e8
Shane O'Moore <shaneiadt@gmail.com>
2021-11-20 13:44:25 +0000

    flv.js player

--- streams/client/src/components/Streams/StreamShow.js
+++ streams/client/src/components/Streams/StreamShow.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { Loader } from "semantic-ui-react";
+import flv from 'flv.js';
 
 import { fetchStream } from "../../actions";
 
 class StreamShow extends Component {
+    videoRef = React.createRef();
+
     componentDidMount() {
-        this.props.fetchStream(this.props.match.params.id);
+        const { match: { params: { id } } } = this.props;
+
+        this.props.fetchStream(id);
+        this.buildPlayer();
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer = () => {
+        if (this.player || !this.props.stream) return;
+
+        const { match: { params: { id } } } = this.props;
+
+        this.flyPlayer = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+
+        this.flyPlayer.attachMediaElement(this.videoRef.current);
+        this.flyPlayer.load();
     }
 
     render() {
@@ -16,6 +40,7 @@ class StreamShow extends Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
