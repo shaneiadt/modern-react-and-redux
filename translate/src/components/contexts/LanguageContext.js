0c35a7871c2f445f95db0ab35c4d21aec9e6a0e6
Shane O'Moore <shaneiadt@gmail.com>
2021-11-23 13:46:38 +0000

    setup language store

--- translate/src/components/contexts/LanguageContext.js
+++ translate/src/components/contexts/LanguageContext.js
@@ -1,3 +1,21 @@
-import { createContext } from "react";
+import { Component, createContext } from "react";
 
-export default createContext('english');
+const Context = createContext('english');
+
+class LanguageStore extends Component {
+    state = {
+        language: 'english'
+    };
+
+    onLanguageChange = (language) => this.setState({ language });
+
+    render() {
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export default LanguageStore;
